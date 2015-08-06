94bc4bda18a92e8799f69189a009b1e6358d55bc
Takuya.Okamoto <blackn.red42@gmail.com>
2015-08-06 17:48:53 +0900

    Initial Commit

---
+++ SubmitTransition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SubmitTransition
+//
+//  Created by Takuya Okamoto on 2015/08/06.
+//  Copyright (c) 2015年 Uniface. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
